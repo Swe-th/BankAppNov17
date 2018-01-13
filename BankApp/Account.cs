3a463a16557ed2ff8891aff1df1a3ef8c9a0f9b0
Swethayn <swethayn@outlook.com>
2018-01-12 19:19:33 -0800

    Database added and Migrated

---
+++ BankApp/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+
+    public enum TypeOfAccount
+    {
+        Checking,
+        Savings,
+        CD,
+        Loan
+    }
+    public class Account
+    {
+       /*
+        #region statics
+        private static int lastAccountNumber = 0;
+        #endregion
+        */
+
+        #region Properties
+        [Key]
+        public int AccountNumber { get; private set; }
+        [StringLength(100,ErrorMessage ="Email Address should be of 50  characters in length")]
+        public string EmailAddress { get; set; }
+        public string AccountName { get; set; }
+        public TypeOfAccount AccountType { get; set; }
+        public decimal Balance { get; private set; }
+        #endregion
+
+        #region Constructor
+        public Account()
+        {
+            
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into the account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        /// <summary>
+        /// Withdraw money from the account
+        /// </summary>
+        /// <param name="amount">Amount to be withdrawed</param>
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+    }
+}
---
+++ BankApp/Bank.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    static class Bank
+    {
+        private static BankModel db = new BankModel();
+        //private static List<Account> accounts = new List<Account>();
+
+        public static Account CreateAccount(string emailAddress, string accountName = "Default Account", TypeOfAccount accountType = TypeOfAccount.Checking)
+        {
+
+
+            var account = new Account
+            {
+                EmailAddress = emailAddress,
+                AccountName = accountName,
+                AccountType = accountType
+            };
+
+            db.Accounts.Add(account);
+            db.SaveChanges();
+            //accounts.Add(account);
+            return account;
+        }
+
+        
+        public static List<Account> GetAllAccounts(string emailAddress)
+        {
+            return db.Accounts.Where(a => a.EmailAddress == emailAddress).ToList();
+        }
+
+
+        public static List<Transaction>GetAllTransactions(int accountNumber)
+        {
+            return db.Transactions.Where(t => t.AccountNumber == accountNumber).OrderByDescending(t=>t.TransactionDate).ToList();
+        }
+
+        public static void Deposit(int accountNumber, decimal amount)
+        {
+            var account = db.Accounts.Where(a => a.AccountNumber == accountNumber).FirstOrDefault();
+            if (account != null)
+            {
+                account.Deposit(amount);
+                var transaction = new Transaction
+                {
+                    TransactionDate = DateTime.Now,
+                    TypeofTransaction = TransactionType.Credit,
+                    TansactionAmount = amount,
+                    Description = "Deposit in a branch",
+                    AccountNumber = account.AccountNumber
+                };
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+            }
+        }
+        public static void Withdraw(int accountNumber, decimal amount)
+        {
+            var account = db.Accounts.Where(a => a.AccountNumber == accountNumber).FirstOrDefault();
+            if (account != null)
+            {
+                account.Withdraw(amount);
+                db.SaveChanges();
+            }
+        }
+    }
+}
